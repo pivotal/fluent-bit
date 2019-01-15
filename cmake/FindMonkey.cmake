dc536d6658b33c8f3f552b64fa3d06aade4c6375
Eduardo Silva <eduardo@treasure-data.com>
2019-01-15 14:49:02 -0600

    build: cmake: monkey include dir fix ?
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- cmake/FindMonkey.cmake
+++ cmake/FindMonkey.cmake
@@ -8,12 +8,14 @@
 #  - MONKEY_FOUND      : source code found
 #  - MONKEY_INCLUDE_DIR: root include directory
 
-unset(MONKEY_INCLUDE_DIR CACHE)
-find_path(MONKEY_INCLUDE_DIR
-  NAMES monkey/mk_core.h monkey/mk_lib.h
-  PATHS ${PROJECT_SOURCE_DIR}/lib/monkey/include
-  )
+#unset(MONKEY_INCLUDE_DIR CACHE)
+#find_path(MONKEY_INCLUDE_DIR
+#  NAMES monkey/mk_core.h monkey/mk_lib.h
+#  PATHS ${PROJECT_SOURCE_DIR}/lib/monkey/include
+#  )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Monkey DEFAULT_MSG MONKEY_INCLUDE_DIR)
-include(FeatureSummary)
+#include(FindPackageHandleStandardArgs)
+#find_package_handle_standard_args(Monkey DEFAULT_MSG MONKEY_INCLUDE_DIR)
+#include(FeatureSummary)
+
+set(MONKEY_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/lib/monkey/include")
