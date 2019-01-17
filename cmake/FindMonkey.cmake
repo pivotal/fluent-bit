2c06e68d94c122c679831e6db60dacd527f170da
Eduardo Silva <eduardo@treasure-data.com>
2019-01-17 14:16:08 -0600

    build: cmake: monkey: try ONLY_CMAKE_FIND_ROOT_PATH
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- cmake/FindMonkey.cmake
+++ cmake/FindMonkey.cmake
@@ -8,14 +8,15 @@
 #  - MONKEY_FOUND      : source code found
 #  - MONKEY_INCLUDE_DIR: root include directory
 
-#unset(MONKEY_INCLUDE_DIR CACHE)
-#find_path(MONKEY_INCLUDE_DIR
-#  NAMES monkey/mk_core.h monkey/mk_lib.h
-#  PATHS ${PROJECT_SOURCE_DIR}/lib/monkey/include
-#  )
+unset(MONKEY_INCLUDE_DIR CACHE)
+find_path(MONKEY_INCLUDE_DIR
+  NAMES monkey/mk_core.h monkey/mk_lib.h
+  PATHS ${PROJECT_SOURCE_DIR}/lib/monkey/include
+  ONLY_CMAKE_FIND_ROOT_PATH
+  )
 
-#include(FindPackageHandleStandardArgs)
-#find_package_handle_standard_args(Monkey DEFAULT_MSG MONKEY_INCLUDE_DIR)
-#include(FeatureSummary)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Monkey DEFAULT_MSG MONKEY_INCLUDE_DIR)
+include(FeatureSummary)
 
-set(MONKEY_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/lib/monkey/include")
+#set(MONKEY_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/lib/monkey/include")
